33bbb17ffddecf6278da8c0947ec4c27948e121d
Guilherme Oliveira <72478821+GHzOliveira@users.noreply.github.com>
2023-12-05 10:03:42 -0300

    Juntando com back-end

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  // baseURL: 'http://10.35.93.41:3333',
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://localhost:3000',
 })
 
 export default http
